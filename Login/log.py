750aa6d7465bb4705f55f5f30371cd62ca4d00bb
Eduardo Vazquez <eduardovazquez8895@gmail.com>
2021-01-08 13:46:14 -0500

    add archivo log

---
+++ Login/log.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import*
+from tkinter import messagebox
+import pymysql
+
+pantalla=Tk()
+pantalla.geometry("300x380")
+pantalla.title("bienvenidos")
+pantalla.iconbitmap("fci.ico")
+
+pantalla.mainloop()
