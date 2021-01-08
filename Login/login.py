1f8072c30f78717c739e55ae62cd2124f86403f0
Eduardo Vazquez <eduardovazquez8895@gmail.com>
2021-01-08 12:18:03 -0500

    pantalla de inicio

---
+++ Login/login.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import*
+from tkinter import messagebox
+import pymysql
+
+pantalla=Tk()
+pantalla.geometry("300*380")
+pantalla.title("bienvenidos")
+
+pantalla.mainloop()
