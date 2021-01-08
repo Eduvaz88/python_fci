90c4c4271414b56158aaa87172273d97b677e07d
Eduardo Vazquez <eduardovazquez8895@gmail.com>
2021-01-08 13:43:55 -0500

    actualizacion de inicio

--- Login/login.py
+++ Login/login.py
@@ -4,7 +4,8 @@ from tkinter import messagebox
 import pymysql
 
 pantalla=Tk()
-pantalla.geometry("300*380")
+pantalla.geometry("300x380")
 pantalla.title("bienvenidos")
+pantalla.iconbitmap("fci.ico")
 
 pantalla.mainloop()
