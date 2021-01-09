ba326113109fa077c49e3e27cddc969ddff222fd
Eduardo Vazquez <eduardovazquez8895@gmail.com>
2021-01-09 14:35:29 -0500

    act

--- Login/log.py
+++ Login/log.py
@@ -5,7 +5,17 @@ import pymysql
 
 pantalla=Tk()
 pantalla.geometry("300x380")
-pantalla.title("FCI")
+pantalla.title("bienvenidos")
 pantalla.iconbitmap("fci.ico")
+
+image=PhotoImage(file="fci.gif")
+image=image.subsample(2,2)
+label=Label(image=image)
+label.pack()
+
+Label(text="Acceso al Sistema", bg="navy", fg="white", width=300, heigth=3, font=("Calibri", 15)).pack()
+Label(text="").pack()
+
+print("hola mundo")
 
 pantalla.mainloop()
