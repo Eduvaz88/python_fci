7a2f26665de8d9416eb5dd3be224c129eb537090
Eduardo Vazquez <eduardovazquez8895@gmail.com>
2021-01-11 15:52:41 -0500

    pestanas html

--- App.py
+++ App.py
@@ -13,6 +13,15 @@ mysql = MySQL(app)
 def Index():
     return render_template('index.html')
 
+@app.route('/ad_pelicula')
+def Pelicula():
+    return render_template('ad_pelicula.html')
+
+@app.route('/ad_rating')
+def Rating():
+    return render_template('ad_rating.html')
+
+
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
   if request.method == 'POST':
@@ -24,6 +33,29 @@ def add_contact():
       (nombre, telefono, email))
       mysql.connection.commit()
       return redirect(url_for('Index'))
+
+@app.route('/ad_pelicula', methods=['POST'])
+def ad_pelicula():
+  if request.method == 'POST':
+      nombre = request.form['nombre']
+      duracion = request.form['duracion']
+      cur = mysql.connection.cursor()
+      cur.execute('INSERT INTO pelicula (nombre, duracion) VALUES (%s, %s)',
+      (nombre, duracion))
+      mysql.connection.commit()
+      return redirect(url_for('Pelicula'))
+
+@app.route('/ad_rating', methods=['POST'])
+def ad_rating():
+  if request.method == 'POST':
+      idpelicula = request.form['idpelicula']
+      idcliente = request.form['idcliente']
+      rating = request.form['rating']
+      cur = mysql.connection.cursor()
+      cur.execute('INSERT INTO rating (idpelicula, idcliente, rating) VALUES (%s, %s, %s)',
+      (idpelicula, idcliente, rating))
+      mysql.connection.commit()
+      return redirect(url_for('Rating'))
 
 
 if __name__ == '__main__':
