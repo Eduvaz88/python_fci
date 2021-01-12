449861edd08c88626220b4069490699cd49570b0
Eduardo Vazquez <eduardovazquez8895@gmail.com>
2021-01-12 01:03:28 -0500

    edit,delete,update en cliente

--- App.py
+++ App.py
@@ -2,16 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+
+#conexion con la bd
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'proyecto_pa'
 mysql = MySQL(app)
 
+#configuraciones
+app.secret_key = 'mysecretkey'
 
+
+#rutas
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM cliente')
+    data = cur.fetchall()
+    return render_template('index.html', cliente = data)
 
 @app.route('/ad_pelicula')
 def Pelicula():
@@ -32,6 +42,7 @@ def ad_cliente():
       cur.execute('INSERT INTO cliente (cedula, nombre, apellido) VALUES (%s, %s, %s)',
       (cedula, nombre, apellido))
       mysql.connection.commit()
+      flash("Cliente agredado satisfactoriamente")
       return redirect(url_for('Index'))
 
 @app.route('/ad_pelicula', methods=['POST'])
@@ -56,6 +67,39 @@ def ad_rating():
       (idpelicula, idcliente, rating))
       mysql.connection.commit()
       return redirect(url_for('Rating'))
+
+@app.route('/delete/<string:id>')
+def delete_cli(id):
+  cur = mysql.connection.cursor()
+  cur.execute('DELETE FROM cliente WHERE id = {0}'.format(id))
+  mysql.connection.commit()
+  flash('Cliente eliminado')
+  return redirect(url_for('Index'))
+
+@app.route('/edit/<id>')
+def get_cliente(id):
+  cur = mysql.connection.cursor()
+  cur.execute('SELECT * FROM cliente WHERE id = %s', (id))
+  data = cur.fetchall()
+  return render_template('edit_clie.html', cliente = data[0])
+
+@app.route('/update/<id>', methods = ['POST'])
+def update_cli(id):
+  if request.method == 'POST':
+    cedula = request.form['cedula']
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    cur = mysql.connection.cursor()
+    cur.execute("""
+      UPDATE cliente
+      SET cedula = %s,
+          nombre = %s,
+          apellido = %s
+      WHERE id = %s
+    """, (cedula, nombre, apellido))
+    mysql.connection.commit()
+    flash('datos editados')
+    return redirect(url_for('Index'))
 
 
 if __name__ == '__main__':
