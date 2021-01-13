9b496b60deef60f4b49aecba46458b97a3393ed0
Eduardo Vazquez <eduardovazquez8895@gmail.com>
2021-01-13 00:24:50 -0500

    pinche clave del root"

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #conexion con la bd
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'proyecto_pa'
 mysql = MySQL(app)
 
